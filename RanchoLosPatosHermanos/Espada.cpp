a62bed41fc6b29732ca97be933bbce21b8580288
javman18 <69351868+javman18@users.noreply.github.com>
2020-08-11 08:07:35 -0500

    hice espada

---
+++ RanchoLosPatosHermanos/Espada.cpp
@@ -0,0 +1,8 @@
+#include "Espada.h"
+
+Espada::Espada()
+{
+	SetPrecio(150);
+	SetNombre("Espada");
+	SetRutaImagen("assets/espada.png");
+}
---
+++ RanchoLosPatosHermanos/Pala.cpp
@@ -0,0 +1,9 @@
+#include "Pala.h"
+
+Pala::Pala()
+{
+
+	SetPrecio(150);
+	SetNombre("Pala");
+	SetRutaImagen("assets/pala.png");
+}
