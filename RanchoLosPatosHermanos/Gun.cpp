40d22f391356626f97d336dc1fb518a5c16ed679
Gabriel <gabrielvega1230@gmail.com>
2020-08-11 07:57:44 -0500

    agrege clase de arma

---
+++ RanchoLosPatosHermanos/Gun.cpp
@@ -0,0 +1,7 @@
+#include "Gun.h"
+
+Gun::Gun() {
+	SetPrecio(10000);
+	SetNombre("Gun");
+	SetRutaImagen("assets/gun.png");
+}
---
+++ RanchoLosPatosHermanos/Gun.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Item.h"
+class Gun :
+	public Item
+{
+public:
+	Gun();
+};
+
