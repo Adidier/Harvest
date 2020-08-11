9da7da1a118694262fb4ecdf1d8bfb12776d41c4
Marco Herrera <herreragdl@hotmail.com>
2020-08-11 08:20:20 -0500

    configure cuerda

---
+++ RanchoLosPatosHermanos/Cuerda.cpp
@@ -0,0 +1,8 @@
+#include "Cuerda.h"
+
+Cuerda::Cuerda()
+{
+	SetPrecio(50);
+	SetNombre("Cuerda");
+	SetRutaImagen("assets/cuerda.png");
+}
---
+++ RanchoLosPatosHermanos/Cuerda.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Item.h"
+class Cuerda :
+	public Item
+{
+public:
+	Cuerda();
+};
+
